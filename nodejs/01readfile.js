55b5be13bc344c454e6be63a5641f279589743b0
sarveshhome <sarvesh.carlton@gmail.com>
2016-09-14 06:33:00 +0100

    init commit

---
+++ nodejs/01readfile.js
@@ -0,0 +1,17 @@
+fs = require('fs')
+//if file not exits
+fs.readFile('/file/hosts', 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
+
+
+//if file exists
+fs.readFile('././file/data.txt', 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
