50e598824dd601096f50c6943a75993b884492fd
sarveshhome <sarvesh.carlton@gmail.com>
2016-12-17 05:23:26 +0000

    file upload in node js
    file upload in node js

---
+++ Fileupload/node-formidable.js
@@ -0,0 +1,28 @@
+var formidable = require('formidable'),
+    http = require('http'),
+    util = require('util');
+http.createServer(function(req, res) {
+    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+                // errors 
+                console.error(err.message);
+                return;
+            }
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.write('received upload:\n\n');
+
+            res.end(util.inspect({ fields: fields, files: files }));
+        });
+        return;
+    }
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.end(
+        '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        '<input type="text" name="title"><br>' +
+        '<input type="file" name="upload" multiple="multiple"><br>' +
+        '<input type="submit" value="Upload">' +
+        '</form>'
+    );
+}).listen(8080);
